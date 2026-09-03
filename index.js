8a7a76faeb24fe32c7695c7d2133b55b2b351a15
Asmaa Alabdulhadi <AAlabdulhadi@mohe.edu.kw>
2026-09-03 13:17:24 +0300

    code with more features and grid layout

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 const Doaa_API_URL = "https://islamicapi.com/api/v1/ruqyah/?type=instant&lang=en&program=a-long-ruqya&source=from-quran&random=true&api_key=W2SPdhx5Hn9AxBRpUC4dRV0T9sA0yUENALBlFZmqWjL76CVK";
 const Weather_API = "http://api.airvisual.com/v2/nearest_city?key=3391f13b-f181-441c-9127-ae3438458152";
+const Hijri_Date ="https://api.aladhan.com/v1/hToG/01-07-1446?calendarMethod=UAQ";
+const date = new Date();
+// Outputs as DD/MM/YYYY (e.g., "02/09/2026")
+const formattedDate = date.toLocaleDateString("en-GB"); 
+const TimeNow = date.toTimeString();
+console.log(TimeNow); 
 
 
 app.get("/", async (req, res) => {
@@ -19,11 +25,22 @@ app.get("/", async (req, res) => {
   const result2 = await axios.get(Weather_API);
       console.log("eesult2: ",result2.data.data.current.weather.tp);
 
+  const result3 = await axios.get(Hijri_Date);
+      console.log("result3: ",result3.data.data.hijri.date);
+
+
 
     res.render("index.ejs", {
       doaa: result.data.data.arabic,
       doaaSource: result.data.data.sub_category,
       tempratureNow: result2.data.data.current.weather.tp,
+      HijriDay: result3.data.data.hijri.day,
+      HijriMonth: result3.data.data.hijri.month.ar,
+      HijriYear: result3.data.data.hijri.year,
+      Timern: TimeNow,
+
+
+      date: formattedDate,
     });
   } catch (error) {
     //console.log(error.response.data);
